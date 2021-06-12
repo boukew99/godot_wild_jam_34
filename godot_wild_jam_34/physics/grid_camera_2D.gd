80cb2b126f64391441688418ee0f01dd405b89eb
Bouke  Westra <boukewestra0@gmail.com>
2021-06-12 14:54:02 +0200

    godot

---
+++ godot_wild_jam_34/physics/grid_camera_2D.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+var block_size = Vector2(ProjectSettings.get("display/window/size/width"), ProjectSettings.get("display/window/size/height"))
+onready var target = get_parent()
+
+func _ready():
+	set_as_toplevel(true)
+	offset = block_size / 2
+func _process(delta):
+	var block_position = (target.position / block_size).floor() 
+	position = block_position * block_size
---
+++ godot_wild_jam_34/physics/space_ship.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+var gas := false
+export var rotation_accel := 15 #low for drunkenness
+
+export(Vector2) var velocity : Vector2 #starting velocity, .length() holds current speed
+func set_velocity(x):
+	set_deferred("velocity", x)
+
+var steering : Vector2
+
+export(float) var acceleration = 2
+export(float) var de_acceleration = 0.6
+export(float) var speed : int = 500 #max_speed
+
+func _physics_process(delta):
+	velocity = forward_steer(get_direction() + transform.x * int(gas), delta)
+	if Input.is_action_just_pressed("gas"): #dash
+		velocity += transform.x * 100
+	if velocity:
+		rotation = lerp_angle(rotation, velocity.angle(), rotation_accel * delta) #for 180 case, turn off for aiming?
+	move_and_slide(velocity)
+
+func get_direction():
+	return Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	).clamped(1)
+	
+	
+func forward_steer(steer_direction : Vector2, delta : float): #no clamp safety
+	steering = steer_direction * speed
+	var next_velocity : Vector2
+
+	if not steer_direction: #steering.dot(velocity) <= 0: #de_accelerate / stop when steering backwards or no steering
+		next_velocity = velocity.linear_interpolate(steering, de_acceleration * delta)
+		if velocity.length() < 5: #and not steer_direction
+			next_velocity = Vector2.ZERO
+	else: #accelerate
+		next_velocity = velocity.linear_interpolate(steering, acceleration * delta)
+
+#	print(next_velocity.length()) #test velocity speed
+	return next_velocity
