3bd4a0ed819ff5c2c2a015bcefc80f42473e3096
Bouke  Westra <boukewestra0@gmail.com>
2021-06-13 01:14:30 +0200

    del print

--- godot_wild_jam_34/main.gd
+++ godot_wild_jam_34/main.gd
@@ -7,7 +7,6 @@ func _ready():
 	minimap = get_node(minimap) as GridContainer
 	
 func _on_GridCamera2D_position_changed(block_postion):
-	print(block_postion)
 	minimap.get_child(index).modulate.r = 1
 	index = wrapi(block_postion.x, 0, 2) + wrapi( block_postion.y  , 0, 2) * 2
 	minimap.get_child(index).modulate.r = 0.5
