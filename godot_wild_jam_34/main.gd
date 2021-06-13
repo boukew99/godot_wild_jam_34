1f7238c9e4b376a987287b67a6841187ad4ee5e6
Bouke  Westra <boukewestra0@gmail.com>
2021-06-13 14:28:58 +0200

    bigger map, no space loop anymore, and a "place"
    space loop screwed with collision

--- godot_wild_jam_34/main.gd
+++ godot_wild_jam_34/main.gd
@@ -1,13 +1,16 @@
 extends Node
 
 var index := 0
+
+export(NodePath) var rooms
 export(NodePath) var minimap
 
 func _ready():
+	rooms = get_node(rooms)
 	minimap = get_node(minimap) as GridContainer
 	
 func _on_GridCamera2D_position_changed(block_postion):
 	minimap.get_child(index).modulate.r = 1
-	index = wrapi(block_postion.x, 0, 2) + wrapi( block_postion.y  , 0, 2) * 2
+	index = wrapi(block_postion.x, 0, rooms.columns) + wrapi(block_postion.y, 0, rooms.rows) * rooms.columns
 	minimap.get_child(index).modulate.r = 0.5
 	
