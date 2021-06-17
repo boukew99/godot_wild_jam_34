60b5d3fddf6538ef27ec1acad1b976a6cd760d4c
Bouke  Westra <boukewestra0@gmail.com>
2021-06-17 23:48:05 +0200

    working a little intro

--- godot_wild_jam_34/main.gd
+++ godot_wild_jam_34/main.gd
@@ -1,27 +1,15 @@
 extends Node
 
-var index := 0
-
-export(NodePath) var rooms
-export(NodePath) var minimap
-
-func _ready():
-	rooms = get_node(rooms)
-	minimap = get_node(minimap) as GridContainer
-	get_tree().paused = true
-	$TitleLayer/Titlescreen.grab_focus()
-	
-func _on_GridCamera2D_position_changed(block_postion):
-	minimap.get_child(index).modulate = Color.white
-	index = wrapi(block_postion.x, 0, rooms.columns) + wrapi(block_postion.y, 0, rooms.rows) * rooms.columns
-	minimap.get_child(index).modulate = Color.darkgray
-	
+export(PackedScene) var game
 
 func _unhandled_input(event):
 	if event.is_action_pressed("fullscreen"):
 		OS.window_fullscreen = not OS.window_fullscreen
 
-
-func _on_Titlescreen_pressed():
-	$TitleLayer.queue_free()
-	get_tree().paused = false
+func _on_BootCatch_pressed():
+	$Intro/AnimationPlayer.play("acts")
+	
+func _on_AnimationPlayer_animation_finished(anim_name):
+	var g = game.instance()
+	add_child(g)
+	
--- godot_wild_jam_34/physics/gui/screen_grid_container.gd
+++ godot_wild_jam_34/physics/gui/screen_grid_container.gd
@@ -8,7 +8,7 @@ onready var rows = get_child_count() / columns
 func _notification(what):
 	if (what==NOTIFICATION_SORT_CHILDREN):
 		rect_min_size.x = screen_size.x * columns
-		rect_min_size.y = screen_size.y *  int(get_child_count() % columns)
+		rect_min_size.y = screen_size.y *  rows
 		for index in get_child_count():
 			var pos = Vector2(index % columns, index / columns) * screen_size
 			
