10ee39994089c61c25f7154880622b8055531c15
Bouke  Westra <boukewestra0@gmail.com>
2021-06-18 13:24:36 +0200

    controller support and laser particles

--- godot_wild_jam_34/physics/2d/laser.gd
+++ godot_wild_jam_34/physics/2d/laser.gd
@@ -2,6 +2,7 @@ extends Position2D
 
 onready var ray_cast = $RayCast2D
 onready var line = $Line2D
+onready var end = $Line2D/End
 
 func _unhandled_input(event):
 	if event.is_action_pressed("laser"):
@@ -10,12 +11,13 @@ func _unhandled_input(event):
 		$Timer.start()
 		
 func _physics_process(delta):
+	var distance = ray_cast.cast_to.x
 	if ray_cast.is_colliding():
-		line.points[1].x = (ray_cast.get_collision_point() - global_position).length()
-	else:
-		line.points[1].x = ray_cast.cast_to.x
+		distance = (ray_cast.get_collision_point() - global_position).length()
 
-
+	line.points[1].x = distance
+	end.position.x = distance
+	
 func _on_Timer_timeout():
 	line.hide()
 	ray_cast.enabled = false
