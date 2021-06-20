d3cebb1f7998dec7ad48b662e5e0089dc95b3690
pompy <pompyproduction@gmail.com>
2021-06-20 20:18:09 +0100

    finished intro sequence

--- godot_wild_jam_34/main.gd
+++ godot_wild_jam_34/main.gd
@@ -5,9 +5,11 @@ const scene_game = preload("res://physics/2d/game.tscn")
 const scene_intro = preload("res://physics/gui/intro.tscn")
 
 var current_scene
+var next_scene
 
 func _ready():
 	current_scene = $Splash
+	current_scene.connect("start_game", self, "start_game")
 
 func _unhandled_input(event):
 	if event.is_action_pressed("fullscreen"):
@@ -21,11 +23,29 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 	add_child(g)
 	
 func switch_scene(scene_res):
-	var newscene
+	var new_scene
 	match scene_res:
 		"intro":
-			newscene = scene_intro.instance()
+			new_scene = scene_intro.instance()
+			new_scene.connect("change_scene", self, "switch_scene")
 		"game":
-			newscene = scene_game.instance()
-	newscene
-	add_child(newscene)
+			new_scene = scene_game.instance()
+	next_scene = new_scene
+	$Tween.interpolate_property(current_scene, "modulate", 
+		Color(1, 1, 1, 1), Color(1, 1, 1, 0), 
+		0.5, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	$Tween.start()
+
+func _on_Tween_tween_completed(object, key):
+	if current_scene != next_scene:
+		add_child(next_scene)
+		next_scene.modulate.a = 0
+		current_scene.queue_free()
+		current_scene = next_scene
+		$Tween.interpolate_property(current_scene, "modulate", 
+			Color(1, 1, 1, 0), Color(1, 1, 1, 1), 
+			0.5, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+		$Tween.start()
+
+func start_game():
+	switch_scene("intro")
--- godot_wild_jam_34/physics/2d/splash.gd
+++ godot_wild_jam_34/physics/2d/splash.gd
@@ -3,6 +3,8 @@ extends Node2D
 var state = "init"
 const res_titlebox = preload("res://physics/gui/titlebox.tscn")
 
+signal start_game
+
 func _on_Timer_timeout():
 	match state:
 		"init":
@@ -19,5 +21,16 @@ func _unhandled_key_input(event):
 
 func draw_titlebox():
 	var box = res_titlebox.instance()
+	box.connect("start_game", self, "start_game")
+	box.connect("popup", self, "draw_popup")
 	add_child(box)
 	
+func start_game():
+	emit_signal("start_game")
+
+func draw_popup(window):
+	match window:
+		"controls":
+			pass
+		"credits":
+			pass
