7c890761de0efda85557ff48f045d45c6ed50160
Bouke  Westra <boukewestra0@gmail.com>
2021-06-20 23:05:58 +0200

    laser target code

--- godot_wild_jam_34/physics/2d/object/laser_target.gd
+++ godot_wild_jam_34/physics/2d/object/laser_target.gd
@@ -4,6 +4,7 @@ extends StaticBody2D
 export var active_time := 10.0 # 0 = not timer
 signal on_toggle(active)
 onready var timer = $Timer
+onready var animation_player = $AnimationPlayer
 
 func _ready():
 	if active_time: timer.wait_time = active_time
@@ -11,8 +12,10 @@ func _ready():
 func _on_laser_hit():
 	if timer.is_stopped():
 		emit_signal("on_toggle", true)
+		animation_player.play("Green")
 		if active_time: timer.start()
-
+			
 
 func _on_Timer_timeout():
 	emit_signal("on_toggle", false)
+	animation_player.play("Red")
