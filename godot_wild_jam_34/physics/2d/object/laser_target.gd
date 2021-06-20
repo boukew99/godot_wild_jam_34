8dbc90b487c7f4a87f94c1ecd5a68417888c99dd
pompy <pompyproduction@gmail.com>
2021-06-20 14:13:09 +0100

    Revert "Merge branch 'main' of https://github.com/boukew99/godot_wild_jam_34 into main"
    This reverts commit 8fedb85c9461b5ee5dae5ffa5e2243b17bd962fb, reversing
    changes made to 6f2907224cd4b63afef1681b839cf4cab4e7502c.

--- godot_wild_jam_34/physics/2d/object/laser_target.gd
+++ godot_wild_jam_34/physics/2d/object/laser_target.gd
@@ -1,17 +1,6 @@
 extends StaticBody2D
 
-export var active_time := 10.0 # 0 = not timer
-signal on_toggle(active)
-onready var timer = $Timer
+signal _on_laser_hit
 
-func _ready():
-	timer.wait_time = active_time
-	
 func _on_laser_hit():
-	if timer.is_stopped():
-		emit_signal("on_toggle", true)
-		if active_time: timer.start()
-
-
-func _on_Timer_timeout():
-	emit_signal("on_toggle", false)
+	emit_signal("_on_laser_hit")
