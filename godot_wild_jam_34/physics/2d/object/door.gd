2c75dad70fb13d9c1f04fb505f9f62858146e2b6
Bouke  Westra <boukewestra0@gmail.com>
2021-06-20 17:06:52 +0200

    door laser on/off

--- godot_wild_jam_34/physics/2d/object/door.gd
+++ godot_wild_jam_34/physics/2d/object/door.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
 onready var collsion_shape = $CollisionShape2D
+onready var line = $Line2D
 
 func on_toggle(active):
 	collsion_shape.disabled = active
+	line.visible = not active
