0999b73d490bac24b0225e8a1c4af9cdaba0f3be
Bouke  Westra <boukewestra0@gmail.com>
2021-06-20 15:47:11 +0200

    the doors work now too

--- godot_wild_jam_34/physics/2d/object/door.gd
+++ godot_wild_jam_34/physics/2d/object/door.gd
@@ -2,8 +2,5 @@ extends StaticBody2D
 
 onready var collsion_shape = $CollisionShape2D
 
-func open():
-	collsion_shape.disabled = true
-	
-func close():
-	collsion_shape.disabled = false
+func on_toggle(active):
+	collsion_shape.disabled = active
