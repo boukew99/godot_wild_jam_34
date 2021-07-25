258fcf5a8af02870db58670ce6e59d14488c0a42
Bouke  Westra <boukewestra0@gmail.com>
2021-07-25 14:14:02 +0200

    undo laser_fix

--- godot_wild_jam_34/physics/2d/object/laser_target.gd
+++ godot_wild_jam_34/physics/2d/object/laser_target.gd
@@ -13,12 +13,10 @@ func _ready():
 func _on_laser_hit():
 	if timer.is_stopped():
 		emit_signal("on_toggle", true)
-		collision_shape.disabled = true
 		animation_player.play("Green")
 		if active_time: timer.start()
 			
 
 func _on_Timer_timeout():
-	collision_shape.disabled = false
 	animation_player.play("Red")
 	emit_signal("on_toggle", false)
