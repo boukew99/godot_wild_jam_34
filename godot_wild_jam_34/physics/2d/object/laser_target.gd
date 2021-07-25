3c1d59d662cd3cdf74a62196ead6b41a29bbbb42
Bouke  Westra <boukewestra0@gmail.com>
2021-07-25 14:12:42 +0200

    intro fade_out fix, laser target retrigger fix

--- godot_wild_jam_34/physics/2d/object/laser_target.gd
+++ godot_wild_jam_34/physics/2d/object/laser_target.gd
@@ -5,6 +5,7 @@ export var active_time := 10.0 # 0 = not timer
 signal on_toggle(active)
 onready var timer = $Timer
 onready var animation_player = $AnimationPlayer
+onready var collision_shape = $CollisionShape2D2
 
 func _ready():
 	if active_time: timer.wait_time = active_time
@@ -12,10 +13,12 @@ func _ready():
 func _on_laser_hit():
 	if timer.is_stopped():
 		emit_signal("on_toggle", true)
+		collision_shape.disabled = true
 		animation_player.play("Green")
 		if active_time: timer.start()
 			
 
 func _on_Timer_timeout():
-	emit_signal("on_toggle", false)
+	collision_shape.disabled = false
 	animation_player.play("Red")
+	emit_signal("on_toggle", false)
--- godot_wild_jam_34/physics/2d/object/pole.gd
+++ godot_wild_jam_34/physics/2d/object/pole.gd
@@ -4,13 +4,17 @@ signal toggled(active)
 
 onready var timer = $Timer
 onready var polygon = $Polygon2D
+onready var collision_shape = $CollisionShape2D
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
 		timer.start()
 		polygon.color = Color(0.6, 0.458824, 0.466667)
+		collision_shape.disabled = true
 		emit_signal("toggled", true)
+		
 		
 func _on_Timer_timeout():
 	polygon.color = Color(0.203922, 0.25098, 0.337255)
 	emit_signal("toggled", false)
+	collision_shape.disabled = false
