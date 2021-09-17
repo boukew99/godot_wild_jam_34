5c8a9a5bfab9ff34299023f9ad29f91e9c9f8e3b
Bouke  Westra <boukewestra0@gmail.com>
2021-09-17 13:15:53 +0200

    last fix

--- project/actor/transition/transition.gd
+++ project/actor/transition/transition.gd
@@ -5,6 +5,7 @@ export var fade_in = true
 export var fade_out = false
 
 func _ready():
+	get_tree().paused = false
 	if fade_in:
 		show() #popup()
 		$AnimationPlayer.play("fade_in")
@@ -17,6 +18,7 @@ func change_scene():
 	if fade_out:
 		var anim = $AnimationPlayer
 		anim.play("fade_out")
+		get_tree().paused = true
 		yield(anim, "animation_finished")
 		
 #	AudioServer.set_bus_effect_enabled(0,0, true)
